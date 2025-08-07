814307507b80dfa336c57a065d62eeba5af44eac
Ester Hugosson <eh225bx@student.lnu.se>
2025-08-07 19:26:28 +0200

    Fix the endpoint for rag queries

--- wine-app/src/service/wineService.js
+++ wine-app/src/service/wineService.js
@@ -5,10 +5,20 @@ const url = import.meta.env.VITE_API_URL
 export const wineService = {
     getAverageCount: async () => {
         try {
-            const response = await axios.get(`${url}/avg-score-by-country`)
+            const response = await axios.get(`${url}/wines/avg-score-by-country`)
             return response.data
         } catch (error) {
             console.error('Error fetching average count:', error)
+            throw error
+        }
+    },
+    askQuestion: async (question) => {
+        try {
+            const response = await axios.post(`${url}/rag/query`, { question: question })
+              console.log("🧠 Received RAG question:", question);
+            return response.data
+        } catch (error) {
+            console.error('Error asking question:', error)
             throw error
         }
     }
