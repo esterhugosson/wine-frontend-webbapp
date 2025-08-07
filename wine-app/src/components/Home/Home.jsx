61a17d28b3fb3dec0e4d31c49e8edd6eedba5c5d
Ester Hugosson <eh225bx@student.lnu.se>
2025-08-07 19:38:20 +0200

    Styling for home page

--- wine-app/src/components/Home/Home.jsx
+++ wine-app/src/components/Home/Home.jsx
@@ -5,12 +5,13 @@ export const Home = () => {
 
     return (
         <div>
-            <h1>The Wine Lab</h1>
+            <h1 className='title'>The Wine Lab</h1>
 
-            <p>Welcome to The Wine Lab, your go-to place for exploring the world of wines. Discover average wine scores, compare prices, and learn about the best varieties.</p>
-            
-            <p>Use the navigation bar to explore different sections of the app.</p>
+            <p className='welcome-text-one'>Welcome to The Wine Lab, your go-to place for exploring the world of wines. Discover average wine scores, compare prices, and learn about the best varieties.</p>
 
+            <p className='welcome-text-2'>Use the navigation bar to explore different sections of the app.</p>
+
+            <h2 className='ask-me'> Ask me something about wine and I might answer</h2>
             <Rag />
 
 
